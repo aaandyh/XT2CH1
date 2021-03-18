cda62a419dbc6df624a2dedf42059241cb0fa31f
aaandyh <61182312+aaandyh@users.noreply.github.com>
2021-03-18 22:50:36 +0100

    Initial commit

--- js/chart.js
+++ js/chart.js
@@ -1,141 +1,53 @@
-function fRader() {
-    var radarChart = document.getElementById('fRadar').getContext('2d');
-
-    Chart.defaults.global.defaultFontSize = 18;
-    Chart.defaults.global.defaultFontColor = '#fff';
-
-    var fRader = new Chart(radarChart, {
-        type:'radar',
-        data:{
-            labels:['Mars', 'Venus', 'Aarde', 'Maan', 'Jupiter','Saturnis'],
-            datasets:[{
-
-                label:'Afstand van Falcon 9 ',
-                data:[65,59,90,81,56,55,40],
-                fill: true,
-                backgroundColor: 'rgba(255, 255, 255, 0.7)',
-                borderColor: 'orange',
-                pointBorderColor: '#000',
-                pointHoverBackgroundColor: '#000',
-                pointHoverBorderColor: '#00',
-            }]
-        },
-        options:{
-            responsive: true,
-            maintainAspectRatio:false,
-            title:{
-                display:true,
-                text: 'Radar',
-                fontStyle: 'bold',
-                fontFamily: 'Helvetica'
-
-            },
-            scale: {
-                angleLines: {
-                    display: true,
-                    color: '#FFF',
-                },
-                gridLines: {
-                    display: true,
-                    color: '#FFF',
-                },
-                ticks: {
-                    fontColor: "white",
-                    fontSize: 18,
-                }
-            },
-            legend:{
-                fontColor: 'black',
-                display:false
-            },
-            tooltips:{
-                enabled:true
-            }
-        }
-    });
+function toonDag(){
+  var vandaag = new Date();
+  var dagen = new Array('zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag');
+  document.getElementById('dag').innerHTML = dagen[vandaag.getDay()];
 }
 
-function brandstofMeter() {
+toonDag();
 
-    var cirkelChart = document.getElementById('brandstof').getContext('2d');
-
-    Chart.defaults.global.defaultFontSize = 14;
-    Chart.defaults.global.defaultFontColor = '#fff';
-
-    var brandstof = new Chart(cirkelChart, {
-        type:'pie',
-        data:{
-            labels:['Waterstof', 'Ethanol', 'Benzine', 'Hydrazine', 'Kerosine','Methaan', 'Propeen'],
-            datasets:[{
-                label:'Afstand van Falcon 9 ',
-                data:[61,87,53,65,58,57,42],
-                fill: true,
-                backgroundColor: ['orange','blue','white','red','green','grey','purple'],
-                borderColor: 'orange',
-                pointBorderColor: '#000',
-                pointHoverBackgroundColor: '#000',
-                pointHoverBorderColor: '#00',
-            }]
-        },
-        options:{
-            responsive: true,
-            maintainAspectRatio:false,
-            title:{
-                display:true,
-                text: 'Brandstof',
-                fontStyle: 'bold',
-                fontFamily: 'Helvetica'
-            },
-            legend:{
-                fontColor: 'black',
-                display:true
-            },
-            tooltips:{
-                enabled:true
-            }
-        }
-    });
+function toonMaand(){
+  var month = new Date();
+  var maanden = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
+  document.getElementById('maand').innerHTML = month.getDate() + ' ' + maanden[month.getMonth()];
 }
 
-function raketInfo() {
-    var raketChart = document.getElementById('raketInfo').getContext('2d');
+toonMaand();
 
-    Chart.defaults.global.defaultFontSize = 14;
-    Chart.defaults.global.defaultFontColor = 'white';
 
-    var raketInfo = new Chart(raketChart, {
-        type:'horizontalBar',
-        data: {
-            labels:['G-Force', 'Acceleratie', 'Zwaartekracht' ],
-            datasets: [{
-                data: [33, 46, 53, 84, 87],
-                backgroundColor: ['blue','grey','red'],
-            }]
-    },
-        options:{
-            responsive: true,
-            maintainAspectRatio:false,
-            title:{
-                display:true,
-                text: 'Raket informatie',
-                fontStyle: 'bold',
-                fontFamily: 'Helvetica'
-            },
-            legend:{
-                fontColor: 'black',
-                display:false
-            },
-            tooltips:{
-                enabled:true
-            }
-        }
-    });
+function toonTijd(){
+  var date = new Date();
+  var uur = date.getHours();
+  var minuut = date.getMinutes();
+  var seconden= date.getSeconds();
+  minuut = checkTime(minuut);
+  seconden = checkTime(seconden);
+
+  document.getElementById('tijd').innerHTML = uur + ":" + minuut + ":" + seconden;
+  var t = setTimeout(toonTijd, 500);
 }
 
-
-function startScript(){
-    fRader();
-    brandstofMeter();
-    raketInfo();
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};
+  return i;
 }
-window.onload = startScript();
+
+toonTijd();
+
+
+function switchDay(){
+var date = new Date();
+var uren = date.getHours();
+
+  if (uren >= 7 && uren < 18) {
+      document.getElementById("nacht").style.display = "none";
+
+  } else if (uren >= 18 && uren < 22) {
+      document.getElementById("daglicht").style.display = "none";
+
+  } else {
+      document.getElementById("daglicht").style.display = "none";
+    }
+}
+
+switchDay();
